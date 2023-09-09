a2a789c322372d5f70e90ef24e65c62f93fd3c09
BianBian2021 <79791035+BianBian2021@users.noreply.github.com>
2023-09-09 21:56:16 +0900

    Update merge_idrs_reads_nig.py

--- merge_idrs_reads_nig.py
+++ merge_idrs_reads_nig.py
@@ -2,7 +2,7 @@ import sys
 import csv
 from pathlib import Path
 
-#python idrs_reads_merge.py E.coli.normalizationbyRNAseq.scaled.Riboseq.codon.wave.adjusted.format.add.codon.filter.different.length.higher60
+
 
 input_wave = sys.argv[1]
 input_path = sys.argv[2]
@@ -14,10 +14,10 @@ with open(input_wave) as f:
         count_reads= lines[2].split(',')
         #print(len(count_reads))
         idrs=[]
-        idrfile = Path("/home/bbian/Data_all/result/IDRs_calculate/iupred2a/"+input_path+"/AF-{}-F1-model_v2.fa.txt".format(uniprotid))
+        idrfile = Path("/Your/work/path/result/IDRs_calculate/iupred2a/"+input_path+"/AF-{}-F1-model_v2.fa.txt".format(uniprotid))
         if not idrfile.is_file():
             continue
-        idrfile = "/home/bbian/Data_all/result/IDRs_calculate/iupred2a/"+input_path+"/AF-{}-F1-model_v2.fa.txt".format(uniprotid)
+        idrfile = "/Your/work/path/result/IDRs_calculate/iupred2a/"+input_path+"/AF-{}-F1-model_v2.fa.txt".format(uniprotid)
         with open (idrfile, "r") as idr:
             for liness in idr.readlines():
                 if liness[0] == "#":
@@ -30,6 +30,6 @@ with open(input_wave) as f:
         if len(idrs)== len(count_reads)-1:
             for i in range (len(count_reads)-1):
                 daf = ("{}\t{}".format(idrs[i], count_reads[i], ))
-                print(daf, file=open("/home/bbian/Data_all_calibrate/result/merge_idrs_reads/"+input_path+"/%s.txt" % uniprotid,"a"))
+                print(daf, file=open("/Your/work/path/result/merge_idrs_reads/"+input_path+"/%s.txt" % uniprotid,"a"))
             else:
                 continue
