2d8673d126193fc99aedf55e41c83ffb2224be12
BianBian2021 <79791035+BianBian2021@users.noreply.github.com>
2023-09-09 22:00:03 +0900

    Update normalized_CO_counts_merge.py

--- normalized_CO_counts_merge.py
+++ normalized_CO_counts_merge.py
@@ -10,20 +10,18 @@ import csv
 import re
 import os
 from pathlib import Path
-# python normalized_CO_counts_merge.py normalized_genename_yeast_K15_Riboseq.codon.wave.txt
+
 
 input_data_wave = sys.argv[1]
 input_path = sys.argv[2]
 
-#data2=[]
-#input_data = sys.argv[1]
+
 with open(input_data_wave) as f:
     for line in f.readlines():
         line1 = line.split()
         geneid = line1[0]
         count_reads = line1[2].split(',')
-        my_file = Path("/home/bbian/Data_all_calibrate/result/CO_normalization/"+input_path+"/{}.norm.txt".format(geneid))
-        #my_file = Path("/Users/bianbian/Desktop/riboseq_tools/translation/normalization_counts_merge/{}.norm.txt".format(geneid))
+        my_file = Path("/Your/work/path/result/CO_normalization/"+input_path+"/{}.norm.txt".format(geneid))
 
         if not my_file.is_file():
             continue
@@ -39,6 +37,6 @@ with open(input_data_wave) as f:
         if len(contact_order) == len(count_reads) - 1:
             for i in range(len(count_reads) - 1):
                 daf = ("{}\t{}".format(contact_order[i], count_reads[i], ))
-                print(daf, file=open("/home/bbian/Data_all_calibrate/result/normalized_absolute_contact_order/"+input_path+"/%s.txt" % geneid, "a"))
+                print(daf, file=open("/Your/work/path/result/normalized_absolute_contact_order/"+input_path+"/%s.txt" % geneid, "a"))
         else:
             continue
