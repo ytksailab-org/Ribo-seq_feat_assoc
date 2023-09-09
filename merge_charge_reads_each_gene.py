03796e8616e708cfbc440b11aa46c8328d51e6df
BianBian2021 <79791035+BianBian2021@users.noreply.github.com>
2023-09-09 21:47:10 +0900

    Update merge_charge_reads_each_gene.py

--- merge_charge_reads_each_gene.py
+++ merge_charge_reads_each_gene.py
@@ -7,7 +7,7 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.PDB import PDBParser
 from Bio.PDB.DSSP import DSSP
-#python extract_positive_charge_merge_read_eachgene.py scale_genename_normalized_ribo_wave_higher60.txt
+#python merge_charge_reads_each_gene.py yeast.scaled.bowtie.Riboseq.codon.wave.adjusted.format.add.codon.filter.different.length.higher60.removed.all
 input_data_wave = sys.argv[1]
 input_path = sys.argv[2]
 with open(input_data_wave) as f:
@@ -19,15 +19,15 @@ with open(input_data_wave) as f:
         amino_acid = []
         charges = []
 
-        chargefile = Path("/home/bbian/Data_all/raw_data/Alpha-fold/"+input_path+"/AF-{}-F1-model_v2.pdb.pepinfo".format(uniprotid))
+        chargefile = Path("/Your/work/path/Alpha-fold/"+input_path+"/AF-{}-F1-model_v2.pdb.pepinfo".format(uniprotid))
         if not chargefile.is_file():
             continue
-        chargefile = "/home/bbian/Data_all/raw_data/Alpha-fold/"+input_path+"/AF-{}-F1-model_v2.pdb.pepinfo".format(uniprotid)
+        chargefile = "/Your/work/path/Alpha-fold/"+input_path+"/AF-{}-F1-model_v2.pdb.pepinfo".format(uniprotid)
 
         t=0
         with open(chargefile,"r") as charge:
             for line in charge.readlines():
-                # <E8><B4><AA><E5><A9><AA><E5><8C><B9><E9><85><8D>Printing out Positive residues
+                #Printing out Positive residues
                 if (re.findall('Printing out Positive residues *?', line)):
                     t = 1
                     continue
@@ -52,6 +52,6 @@ with open(input_data_wave) as f:
         if len(charges) == len(count_reads) - 1:
             for i in range(len(count_reads) - 1):
                 extract_positive = ("{}\t{}\t{}\t{}".format(position[i], amino_acid[i], charges[i], count_reads[i], ))
-                print(extract_positive, file=open("/home/bbian/Data_all_calibrate/result/merge_charge_reads_each_gene/"+input_path+"/%s.txt" % uniprotid, "a"))
+                print(extract_positive, file=open("/Your/work/path/result/merge_charge_reads_each_gene/"+input_path+"/%s.txt" % uniprotid, "a"))
         else:
             continue
